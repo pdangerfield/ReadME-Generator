e01a0d04634cfa5f348a58a0776e34222f7f0f7a
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-04-17 17:12:48 -0600

    added packages needed for project, questions for readme, funcitons to write readme and when initilized.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your Project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use. Include screenshots as needed.",
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators, if any.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license that applies to this project.",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "BSD2", "MPL2.0", "BSD3", "none"] 
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating README.md file...");
+        writeToFile("./Develop/ReadME.md", generateMarkdown({ ...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
